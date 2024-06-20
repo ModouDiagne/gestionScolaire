ead9a63f1ffd4c65df27a6cb1a2794b3be72b9d4
ModouDiagne <diagnemodou7199@gmail.com>
2024-06-20 16:17:36 +0000

    Ajout de nouveaux fichiers et modifications

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NiveauController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/niveaux', [NiveauController::class, 'index'])->name('niveaux');
 });
